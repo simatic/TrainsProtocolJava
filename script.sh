98c69c4c9ff7912edcdef3402f9b16651d647707
arroway <stephanie@minet.net>
2012-12-05 23:40:38 +0100

    script to compile the C library from the ant file (Linux)

--- script.sh
+++ script.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
-CPath="/Users/stephanie/dev/PFE/TrainsProtocol"
+CPath="./TrainsProtocol"
 
 echo "Generating C JNI libraries"
-cd bin/
+cd ./src
 
 echo "generating trains_Interface.h"
 javah -jni trains.Interface
-mv trains_Interface.h $CPath/include/trains_Interface.h
+cd ..
+mv ./src/trains_Interface.h $CPath/include/trains_Interface.h
 
 echo "compiling interface.o"
 cd $CPath/src 
 
 #Ok if we're compiling on Mac OS X 
 #TODO: testing OS 
-gcc -x c -I$CPath/include -I/System/Library/Frameworks/JavaVM.framework/Headers -c interface.c -o interface.o
-echo "compiling libInterface.jnilib"
-gcc -dynamiclib -o libInterface.jnilib interface.o 
+#gcc -x c -I$CPath/include -I/System/Library/Frameworks/JavaVM.framework/Headers -c interface.c -o interface.o
+#echo "compiling libInterface.jnilib"
+#gcc -dynamiclib -o libInterface.jnilib interface.o 
 
-mv libInterface.jnilib $CPath/lib
+#Compiling on Linux
+#gcc -I$JAVA_HOME/include -o libInterface.so -shared interface.c
+make all
+if [ ! -d ../lib ]
+then
+	mkdir ../lib
+fi
+mv libtrains.so ../lib
+LD_LIBRARY_PATH=../lib:LD_LIBRARY_PATH
 
 exit 0;
