c12cc85292489bbfc4973c66104e397bb426b8af
arroway <stephanie@minet.net>
2012-12-11 13:19:37 +0100

    A first version of Example.java

--- src/examples/Example.java
+++ src/examples/Example.java
@@ -1,54 +1,95 @@
 package examples;
 
-import static org.junit.Assert.*;
-import junit.framework.Assert;
-
-import org.junit.Test;
-
-import trains.Interface;
-import trains.CallbackCircuitChange;
-import trains.CallbackUtoDeliver;
-
+import trains.*;
 
 public class Example {
 	
-	@Test
-	public void TestInterface() {
+	private static final class myCallbackCircuitChange implements CallbackCircuitChange{
+				
+		public myCallbackCircuitChange(){
+			//Nothing to do
+		} 
 		
+		@Override
+		public void run(CircuitView cv){
+			//Printing the circuit modification
+			
+			//Printing the new/departed participant
+			if(cv.getJoined() != 0){
+				System.out.println(Integer.toString(cv.getJoined()) + "has arrived.");
+			} else {
+				System.out.println(Integer.toString(cv.getDeparted()) + "is gone.");
+			}
+			
+			//Printing the current number of members
+			System.out.println("Currently " + cv.getMemb() + " in the circuit.");
+		}
+	}
+	
+	private static final class myCallbackUtoDeliver implements CallbackUtoDeliver{
+		
+		public myCallbackUtoDeliver(){
+			//Nothing to do
+		} 
+		
+		@Override
+		public void run(int sender, Message msg){
+			
+			//Printing the message sender and the content upon receiving a message
+			System.out.println("I received a message from " + sender);
+			System.out.println("The content size is " + msg.getMessageHeader().getLen());
+			System.out.println("The content is " + msg.getPayload());
+		}
+	}
+	
+	public static void main() {	
+		
+		//trInit parameters: values by default
+		int trainsNumber = 0;
+		int wagonLength = 0;
+		int waitNb = 0;
+		int waitTime = 0;
+		
+		myCallbackCircuitChange mycallbackCircuit = new myCallbackCircuitChange();
+		myCallbackUtoDeliver mycallbackUto = new myCallbackUtoDeliver();
+		
+		int payload = 0;
+		Message msg = null;
 		int exitcode;
 		
-		//Parameters set by user
-		boolean sender;
-		int nbMemberMin;
-		int delay;
-		int nbRecMsg;
-		
-		//trInit parameters
-		int trainsNumber = 2;
-		int wagonLength = 3;
-		int waitNb = 4;
-		int waitTime = 2;
-		
-		//Error check
-		int status = 0;
-		int errnum = 0;
-	
-		
-		
+		System.out.println("** Load interface");
 		Interface trin = Interface.trainsInterface();
-		
-		//on appelle la jvm que dans ./src/applicationMessage.c
-		
+				
+		System.out.println("** trInit");
 		exitcode = trin.JtrInit(trainsNumber, wagonLength, waitNb, waitTime,
-				(new myCallbackCircuitChange()).getClass().getName(), 
-				(new myCallbackUtoDeliver()).getClass().getName());
-		assertEquals(exitcode, 0);		
+					mycallbackCircuit.getClass().getName(), 
+					mycallbackUto.getClass().getName());
 
-		trin.JtrError_at_line(status, errnum);//, const char *filename, 
-			//	 unsigned int linenum, const char *format){
-		trin.JtrPerror(errnum);
-		exitcode = 1;
+		if (exitcode < 0){
+			System.out.println("JtrInit failed.");
+			return;
+		}
+		
+		payload = 3;
+		
+		//Filling the message
+		System.out.println("** Filling a message");
+		msg = Message.messageFromPayload(String.valueOf(payload));
+		
+		//Needed to keep count of the messages
+		System.out.println("** JnewMsg");
+		trin.JnewMsg(msg.getMessageHeader().getLen());
+		
+		//Sending the message
+		System.out.println("** JutoBroadcast");
+		exitcode = trin.JutoBroadcast(msg);
+		if (exitcode < 0){
+			System.out.println("JutoBroadcast failed.");
+			return;
+		}
+		
+		System.out.println("JtrTerminate");
 		exitcode = trin.JtrTerminate();
-		assertEquals(exitcode, 0);
+		return;
 	}
 }
