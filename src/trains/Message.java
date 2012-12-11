38a988f742b92325cb4c1888bf4c633ac6b2347b
arroway <stephanie@minet.net>
2012-12-11 12:05:53 +0100

    Some clean-up in Message.java

--- src/trains/Message.java
+++ src/trains/Message.java
@@ -1,14 +1,11 @@
 package trains;
 
-/* defined in include/applicationMessage.h */
-
 public class Message {
 
 	private MessageHeader messageHeader;
 	private String payload;
 
 	private Message(MessageHeader msgHdr, String payload){
-		//do something
 		this.messageHeader = msgHdr;
 		this.payload = payload;
 	}
@@ -18,13 +15,11 @@ public class Message {
 	}
 	
 	public static Message messageFromPayloadAndType(String payload, int type){
-		//do something
 		MessageHeader msgHdr = new MessageHeader(payload.length(), type);
 		return new Message(msgHdr, payload);		
 	}
 	
 	public static Message messageFromPayload(String payload){
-		//do something
 		int type = MessageType.AM_BROADCAST.ordinal();
 		MessageHeader msgHdr = MessageHeader.createMessageHeader(payload.length(), type);
 		return new Message(msgHdr, payload);		
