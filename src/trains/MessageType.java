d3d82bea0099134c7d58c99c5445d1e797b17fe8
arroway <stephanie@minet.net>
2012-12-11 11:23:11 +0100

    Add enum class MessageType

---
+++ src/trains/MessageType.java
@@ -0,0 +1,11 @@
+package trains;
+
+public enum MessageType {
+
+	AM_BROADCAST,	/* 0 */
+	AM_ARRIVAL,		/* 1 */
+	AM_DEPARTURE,	/* 2 */
+	AM_TERMINATE,	/* 3 */
+	AM_PING,		/* 4 */
+	AM_PONG;		/* 5 */
+}
