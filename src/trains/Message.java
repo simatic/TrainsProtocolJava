f40eb9b92beb9fd5a6863e8bc9f13be947a82636
arroway <stephanie@minet.net>
2013-01-03 12:55:26 +0100

    comment static factories for Message

--- src/trains/Message.java
+++ src/trains/Message.java
@@ -5,23 +5,28 @@ public class Message {
 	private MessageHeader messageHeader;
 	private String payload;
 
-	private Message(MessageHeader msgHdr, String payload){
+	/* Is there a single Message object in the JVM -> no: 
+	 * 1 created/sent/used by the user
+	 * the 1 that's inside the process (utoDeliveries) 
+	 *  */
+	
+	public Message(MessageHeader msgHdr, String payload){
 		this.messageHeader = msgHdr;
 		this.payload = payload;
 	}
 
-	public static Message createMessage(MessageHeader messageHeader, String payload){
+	public /*static*/ Message createMessage(MessageHeader messageHeader, String payload){
 		return new Message(messageHeader, payload);
 	}
 	
-	public static Message messageFromPayloadAndType(String payload, int type){
+	public /*static*/ Message messageFromPayloadAndType(String payload, int type){
 		MessageHeader msgHdr = new MessageHeader(payload.length(), type);
 		return new Message(msgHdr, payload);		
 	}
 	
-	public static Message messageFromPayload(String payload){
+	public /*static*/ Message messageFromPayload(String payload){
 		int type = MessageType.AM_BROADCAST.ordinal();
-		MessageHeader msgHdr = MessageHeader.createMessageHeader(payload.length(), type);
+		MessageHeader msgHdr = new MessageHeader(payload.length(), type);
 		return new Message(msgHdr, payload);		
 	}
 
