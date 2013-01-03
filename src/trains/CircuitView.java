9c7b5a1558e1689d1b337904f6710c5a6111ad68
arroway <stephanie@minet.net>
2013-01-03 12:56:25 +0100

    CircuitView is a singleton with a static factory

--- src/trains/CircuitView.java
+++ src/trains/CircuitView.java
@@ -2,21 +2,24 @@ package trains;
 
 public class CircuitView {
 
+	/* Singleton with static factory */
+	private static final CircuitView CIRCUITVIEW = new CircuitView(0, 0, 0);
+	
 	private int nmemb; 				/* Number of members */
 	//XXX: should use an Array here
 	private int members_address;	/* List of members */
 	private int joined; 			/* New member */
 	private int departed; 			/* Departed member*/
 	
-	private CircuitView(int memb, int members_address, int joined, int departed){
+	private CircuitView(int memb, int joined, int departed){
 		this.nmemb = memb;
 		this.members_address = members_address;
 		this.joined = joined;
 		this.departed = departed;
 	}
 	
-	public static CircuitView createCircuitView(int memb, int members_address, int joined, int departed){
-		return new CircuitView(memb, members_address, joined, departed);
+	public static CircuitView getInstance(){
+		return CIRCUITVIEW;
 	}
 
 	public void setMemb(int memb){
